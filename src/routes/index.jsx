d6767b07b8ca8a2961f032054d2fe255aa35fbfb
Nguyen Hong Son <snguyenhong8@gmail.com>
2025-09-05 18:46:15 +0700

    update header

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,20 @@
+// Layout
+import { HeaderOnly } from "../components/Layout";
+
+// Pages
 import Home from "../Pages/Home/Home";
 import Following from "../Pages/Following/Following";
 import Profile from "../Pages/Profile/Profile";
+import Upload from "../Pages/Upload/Upload";
+import Search from "../Pages/Search/Search";
 
 // Public routes
 const publicRoutes = [
     {path: '/' , component: Home}, 
     {path: '/following' , component: Following}, 
     {path: '/profile' , component: Profile}, 
+    {path: '/upload' , component: Upload, layout: HeaderOnly}, 
+    {path: '/search' , component: Search, layout: null}, 
 ] 
 
 const privateRoutes = [];
